c3d79c58a3dfd58edafe8a6c0bc2fce0aa8f0400
chatgptitty <158405942+chatgptitty@users.noreply.github.com>
2025-03-13 16:39:43 -0300

    videoDownloader.py

--- modules/videoDownloader.py
+++ modules/videoDownloader.py
@@ -1,13 +1,3 @@
-LICNECE = """
-Copyright © 2021 Drillenissen#4268 - logicguy.mailandcontact@gmail.com
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-"""
-
-__version__ = "1.0"
-__author__  = "Drillenissen#4268"
-
 import modules.utilities as utilities
 from contextlib import contextmanager
 import subprocess
@@ -65,6 +55,3 @@ def main():
 
     else:
         os.remove(f"Video Downloads/{result['uploader']} - {result['title']} - {result['id']}.mp4")
-
-
-#https://www.pornhub.com/view_video.php?viewkey=ph5e80ec51bc6b5
